fcc44fa7c940e8e15002d295c80d312e96c1e76c
aliakbar1881 <aliakbar138118@gmail.com>
2022-05-01 02:19:37 -0400

    add run_init.sh to initialize a python runtime environment

---
+++ pyneed/bash_scripts/run_init.sh
@@ -0,0 +1,9 @@
+clear
+date
+echo -e "Hey $USER;\n\tYour current shell is:  $SHELL "
+echo
+read -p "Press [Enter] key to start run_init.sh . . ."
+export PYTHONPATH=${PWD}
+echo -e "\vYour PYTHONPATH variable is :\n$PYTHONPATH"
+echo
+
